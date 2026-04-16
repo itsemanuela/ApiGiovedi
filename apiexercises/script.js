55e4bf89072ede8b90921e566f338ab1e97a4250
itsemanuela <notemanu152@gmail.com>
2026-04-16 16:58:01 +0200

    Add commenti

--- apiexercises/script.js
+++ apiexercises/script.js
@@ -1,10 +1,12 @@
 const apiKey = "GBpcym3fkbvHBo0S1GLHoJsU9CrhgrF3Zc9T0GQFSMuOGmfhTJmJKmTK";
 
+//ho preso il bottone sul quale volevo gestire l'evento di ascolto per cambiare le immagini ad ogni click
 const bottone1 = document.getElementById("carica-img");
 bottone1.addEventListener("click", (e) => {
   e.preventDefault();
   const url = "https://api.pexels.com/v1/search?query=summer";
 
+  //dopodiche ho fatto una raccolta dati dall'api
   fetch(url, {
     headers: { Authorization: apiKey },
   })
@@ -12,12 +14,13 @@ bottone1.addEventListener("click", (e) => {
       if (!response.ok) throw new Error("Errore server");
       return response.json();
     })
+    //nel secondo blocco the di esito positivo ho effettuato il ciclo foreach per ogni card
     .then((dati) => {
       const immaginihtml = document.querySelectorAll("img");
       immaginihtml.forEach((img, i) => {
         img.src = dati.photos[i].src.medium;
       });
-
+      // qui seleziono tutti gli small per aggiungere l'id contenuta nel json ad ogni card sempre con ciclo foreach
       const small = document.querySelectorAll("small");
       small.forEach((minuti, i) => {
         minuti.innerText = "ID: " + dati.photos[i].id;
@@ -25,7 +28,7 @@ bottone1.addEventListener("click", (e) => {
     })
     .catch((err) => console.log("errore", err));
 });
-
+// ho gestito l'evento sull'altro bottone per mostrare le foto di una risorsa diversa della stessa api
 const different = document.getElementById("query-diversa");
 different.addEventListener("click", (e) => {
   e.preventDefault();
@@ -34,6 +37,7 @@ different.addEventListener("click", (e) => {
   fetch(url2, {
     headers: { Authorization: apiKey },
   })
+    // effettuo stesso procedimento di prima
     .then((response) => {
       if (!response.ok) throw new Error("Errore!!!");
       return response.json();
@@ -52,7 +56,7 @@ different.addEventListener("click", (e) => {
     })
     .catch((err) => console.log("errorissimo"));
 });
-
+// prelevo il bottone delle card facendo riferimento al parent e agisco sull'evento di click eliminando anche la card
 const tasto = document.querySelectorAll(".card .btn-group button:last-child");
 tasto.forEach((bottone) => {
   bottone.innerText = "Hide";
@@ -64,7 +68,7 @@ tasto.forEach((bottone) => {
     }
   });
 });
-
+// ho prelevato la search input dalla barra nav di bootstrap e l'ho modificata
 const barradiricerca = document.getElementById("search");
 barradiricerca.innerHTML = ` <form id="search-form" class="d-flex w-100" >
       <input id= user-query class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -72,7 +76,7 @@ barradiricerca.innerHTML = ` <form id="search-form" class="d-flex w-100" >
     </form>`;
 barradiricerca.style.width = "350px";
 barradiricerca.style.display = "block";
-
+// inizio gestione del form search tramite js
 const form = document.getElementById("search-form");
 const input = document.getElementById("user-query");
 const buttonform = document.getElementById("form-button");
